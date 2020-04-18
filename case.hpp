b3c34ca4465de026c0061bf4dd15b1cfbc8215a9
BARAGAA <moh0916ali@gmail.com>
2020-04-18 21:09:08 +0200

    fixed doublication of case structure

---
+++ case.hpp
@@ -0,0 +1,24 @@
+/**
+ * @author BA RAGAA Mohammed
+ * @date 17/04/2020
+ * @ made as a solution for not doublicating the structure in both of cpp and hpp files of Consigne
+**/
+
+#ifndef DEF_CASE
+#define DEF_CASE
+
+/**
+ * @Role: the alias type of the Bagage which is basically a string
+ * */
+typedef std::string Bagage;
+
+/**
+ * @Role: the alias type of the Bagage which is basically a string
+ * */
+typedef struct{
+        
+        std::size_t place;
+        Bagage package;
+        
+    } Case;
+#endif
